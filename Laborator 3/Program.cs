34974fd992ff21bf5ffbd60b4a476d996c3147a0
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2021-11-18 18:35:35 +0200

    HashGenerator

---
+++ Laborator 3/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator_3
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			if (args == null || args.Length < 2)
+			{
+				Console.WriteLine("HashGenerator v1.1.1");
+				Console.WriteLine("Arg format: -type=<md5 | sha1 | sha256 | sha384 | sha512> (-file=<\"PathToFile\"> | -hash=<HashValue> -search=<\"PathToFolder\">)");
+				return;
+			}
+
+			string hashType = null;
+			string path = null;
+			string hashValue = null;
+			foreach (string arg in args)
+			{
+				var keyValue = arg.Split('=');
+				if (keyValue == null || keyValue.Length != 2)
+				{
+					Console.WriteLine("Invalid arg: " + arg);
+					return;
+				}
+
+				switch (keyValue[0])
+				{
+					case "-type": hashType = keyValue[1]; break;
+					case "-file": case "-search": path = keyValue[1]; break;
+					case "-hash": hashValue = keyValue[1]; break;
+				}
+			}
+
+			if (string.IsNullOrEmpty(hashType))
+			{
+				Console.WriteLine("Error: '-type' not set!");
+				return;
+			}
+
+			if (!string.IsNullOrEmpty(hashValue) && !string.IsNullOrEmpty(path))
+			{
+				Console.WriteLine("Searching...");
+				SearchPathForFileHash(hashType, hashValue, path);
+				Console.WriteLine("Search completed!");
+			}
+			else if (!string.IsNullOrEmpty(path))
+			{
+				Console.WriteLine(GenerateFileHash(hashType, path));
+			}
+			else
+			{
+				Console.WriteLine("Error: '-file' or ('-hash' + '-search') much be set!");
+			}
+		}
+
+		[Pure]
+		private static string GenerateFileHash(string hashType, string pathToFile)
+		{
+			try
+			{
+				using (var stream = new FileStream(pathToFile, FileMode.Open, FileAccess.Read, FileShare.None))
+				{
+					if (hashType == "md5")
+					{
+						using (var hash = new System.Security.Cryptography.MD5CryptoServiceProvider())
+						{
+							var data = hash.ComputeHash(stream);
+							return BitConverter.ToString(data).Replace("-", "").ToLower();
+						}
+					}
+					else if (hashType == "sha1")
+					{
+						using (var hash = new System.Security.Cryptography.SHA1CryptoServiceProvider())
+						{
+							var data = hash.ComputeHash(stream);
+							return BitConverter.ToString(data).Replace("-", "").ToLower();
+						}
+					}
+					else if (hashType == "sha256")
+					{
+						using (var hash = new System.Security.Cryptography.SHA256CryptoServiceProvider())
+						{
+							var data = hash.ComputeHash(stream);
+							return BitConverter.ToString(data).Replace("-", "").ToLower();
+						}
+					}
+					else if (hashType == "sha384")
+					{
+						using (var hash = new System.Security.Cryptography.SHA384CryptoServiceProvider())
+						{
+							var data = hash.ComputeHash(stream);
+							return BitConverter.ToString(data).Replace("-", "").ToLower();
+						}
+					}
+					else if (hashType == "sha512")
+					{
+						using (var hash = new System.Security.Cryptography.SHA512CryptoServiceProvider())
+						{
+							var data = hash.ComputeHash(stream);
+							return BitConverter.ToString(data).Replace("-", "").ToLower();
+						}
+					}
+					else
+					{
+						throw new Exception("Unsupported hashType: " + hashType);
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				return "Error: " + e.Message;
+			}
+		}
+
+		[Pure]
+		private static void SearchPathForFileHash(string hashType, string hash, string searchPath)
+		{
+			try
+			{
+				foreach (string filePath in Directory.GetFiles(searchPath))
+				{
+					string fileHash = GenerateFileHash(hashType, filePath);
+					if (hash == fileHash) Console.WriteLine(string.Format("File found: '{0}'", filePath));
+				}
+
+				foreach (string subSearchPath in Directory.GetDirectories(searchPath))
+				{
+					SearchPathForFileHash(hashType, hash, subSearchPath);
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Error: " + e.Message);
+			}
+		}
+	}
+}
